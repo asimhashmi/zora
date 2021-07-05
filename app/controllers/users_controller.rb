cea3dadd36ac8f83360410bd0dda136ac27dc150
Damilola Omiyera <damilolaomiyera@yahoo.com>
2021-07-05 23:32:47 +0100

    Some dummy search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,17 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_user
 
+  def index
+    @user = User.search(params[:search])
+  end
+
   def show
-      # if Conversation.between(params[:sender_id], params[:recipient_id]).present? 
-      #     @conversation = Conversation.between(params[:sender_id], params[:recipient_id]).first
-      # else
-      #     @conversation = Conversation.create!(conversation_params)
-      # end
-      # redirect_to conversation_messages_path(@conversation)
+    # if Conversation.between(params[:sender_id], params[:recipient_id]).present? 
+    #     @conversation = Conversation.between(params[:sender_id], params[:recipient_id]).first
+    # else
+    #     @conversation = Conversation.create!(conversation_params)
+    # end
+    # redirect_to conversation_messages_path(@conversation)
   end
 
   private
