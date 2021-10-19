a615ab069fb40767a268f74c8d5ca3cb13fd9095
Damilola Omiyera <damilolaomiyera@yahoo.com>
2021-10-19 01:11:07 +0100

    emails after chat

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,7 +5,7 @@ class MessagesController < ApplicationController
     
     def index
         # byebug
-        @messages = @conversation.messages if @conversation.sender_id == current_user.id || @conversation.recipient_id == current_user.id
+        @messages = @conversation.messages if @conversation.sender_id || @conversation.recipient_id == current_user.id
         @message = @conversation.messages.new
         # @message = Message.new(conversation: @conversation)
     end
@@ -17,11 +17,9 @@ class MessagesController < ApplicationController
     def create
         @message = @conversation.messages.new(message_params)
         @user = current_user
-
         if @message.save
             ModelMailer.new_message_notification(@message, current_user.email).deliver_now
-            
-            redirect_to conversation_messages_path(@conversation)
+            redirect_to conversation_messages_path(@conversation), notice: 'Message sent sucessfully.'
         end
     end
 
--- app/mailers/model_mailer.rb
+++ app/mailers/model_mailer.rb
@@ -2,11 +2,16 @@ class ModelMailer < ApplicationMailer
 
     def new_message_notification(message, user)
         # @message_conversation = message.conversation
-        
+        # @user = user
         # users_email = message.project.postusers.map{|postuser| postuser.user.email}.reject{|a| a == current_user_email}.join(",")
         # mail to: users_email, subject: "A new message has been sent!"
         # @user = params[:user]
-        mail(to: "dami@omis.com", subject: '"A new message has been sent!')
+        # mail(to: user.email, subject: 'You have received a new message')
+
+
+        # Fix the email receipent
+        @user = user
+        mail(:to => user, :subject => "You have received a new message")
       end
 
 end
