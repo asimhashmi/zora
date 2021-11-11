b285f7e686a604fc3fd3a7365d11e7758f5a9927
moizafzal936 <moizafzal936@gmail.com>
2021-11-11 23:38:58 +0500

    add configuration to braintree

---
+++ config/initializers/braintree.rb
@@ -0,0 +1,4 @@
+Braintree::Configuration.environment = Zoratutorr.credentials[:braintree][:environment]
+Braintree::Configuration.merchant_id = Zoratutorr.credentials[:braintree][:merchant_id]
+Braintree::Configuration.public_key = Zoratutorr.credentials[:braintree][:public_key]
+Braintree::Configuration.private_key = Zoratutorr.credentials[:braintree][:private_key]
